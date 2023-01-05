e0b5fa6072d5bbdfa5b8106a6dad89b9f12b6598
brawlee <kushalpal109@gmail.com>
2023-01-05 18:59:51 +0530

    Added displayBoard()

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
+void displayBoard(vector<vector<int>> &boardPara)
+{
+	cout<<" "<<boardPara[0][0]<<" | "<<boardPara[0][1]<<" | "<<boardPara[0][2]<<" \n";
+	cout<<"___|___|___\n";
+	cout<<" "<<boardPara[1][0]<<" | "<<boardPara[1][1]<<" | "<<boardPara[1][2]<<" \n";
+	cout<<"___|___|___\n";	
+	cout<<" "<<boardPara[2][0]<<" | "<<boardPara[2][1]<<" | "<<boardPara[2][2]<<" \n";
+	cout<<"   |   |   \n";
+}
 int main()
 {
-	std::cout<<"This is a tictactoe game";
+	cout<<"This is a tictactoe game\n";
+
+	vector<vector<int>> board= {{1,2,3},{4,5,6},{7,8,9}};
+	displayBoard(board);
+	
 }
